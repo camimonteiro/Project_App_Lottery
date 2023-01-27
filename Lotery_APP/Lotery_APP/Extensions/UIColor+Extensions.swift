3b24c3fd3041323189114babe3d2566dfbc7c959
Camille Monteiro <camillemonteiro.dev@gmail.com>
2023-01-27 20:36:59 -0300

    initial files of the Lottery project

---
+++ Lotery_APP/Lotery_APP/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Extensions.swift
+//  Lotery_APP
+//
+//  Created by Cami on 25/01/23.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primaryBlue = UIColor(red: 42/255, green: 99/255, blue: 183/255, alpha: 1.0)
+    
+}
